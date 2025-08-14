f4468293bef5b441a422a10bc88c0e22cdb82ff2
Patrick <pmoore@meshcloud.io>
2025-08-14 20:46:32 +0200

    use existing rg and pull image from acr

--- network.tf
+++ network.tf
@@ -1,13 +1,13 @@
 resource "azurerm_virtual_network" "minio_vnet" {
   name                = "minio-vnet"
   address_space       = [var.vnet_cidr_range]
-  location            = azurerm_resource_group.minio_aci_rg.location
-  resource_group_name = azurerm_resource_group.minio_aci_rg.name
+  location            = data.azurerm_resource_group.minio_aci_rg.location
+  resource_group_name = data.azurerm_resource_group.minio_aci_rg.name
 }
 
 resource "azurerm_subnet" "minio_subnet" {
   name                                          = "minio-subnet"
-  resource_group_name                           = azurerm_resource_group.minio_aci_rg.name
+  resource_group_name                           = data.azurerm_resource_group.minio_aci_rg.name
   virtual_network_name                          = azurerm_virtual_network.minio_vnet.name
   address_prefixes                              = [var.subnet_cidr_range]
   service_endpoints                             = ["Microsoft.Storage"]
@@ -16,7 +16,7 @@ resource "azurerm_subnet" "minio_subnet" {
 
 resource "azurerm_subnet" "minio_ag_subnet" {
   name                 = "minio-ag-subnet"
-  resource_group_name  = azurerm_resource_group.minio_aci_rg.name
+  resource_group_name  = data.azurerm_resource_group.minio_aci_rg.name
   virtual_network_name = azurerm_virtual_network.minio_vnet.name
   address_prefixes     = [var.ag_subnet_cidr_range]
   service_endpoints    = ["Microsoft.Storage"]
@@ -24,8 +24,8 @@ resource "azurerm_subnet" "minio_ag_subnet" {
 
 resource "azurerm_public_ip" "minio_pip" {
   name                = "minio-pip"
-  location            = azurerm_resource_group.minio_aci_rg.location
-  resource_group_name = azurerm_resource_group.minio_aci_rg.name
+  location            = data.azurerm_resource_group.minio_aci_rg.location
+  resource_group_name = data.azurerm_resource_group.minio_aci_rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
