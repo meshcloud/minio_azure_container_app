60f4aba911a54f246fd12bb8e2a3e89ad9dcf76e
Patrick <pmoore@meshcloud.io>
2025-09-04 19:50:19 +0200

    added MINIO_BROWSER_REDIRECT_URL envar

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-# data "azurerm_resource_group" "minio_aci_rg" {
-#   name     = var.resource_group_name
-# }
-
 resource "azurerm_resource_group" "minio_aci_rg" {
   name     = var.resource_group_name
   location = var.location
@@ -12,8 +8,8 @@ resource "azurerm_container_group" "minio_aci_container_group" {
   location            = azurerm_resource_group.minio_aci_rg.location
   resource_group_name = azurerm_resource_group.minio_aci_rg.name
   ip_address_type     = "Private"
-  os_type = "Linux"
-  subnet_ids = [azurerm_subnet.minio_subnet.id]
+  os_type             = "Linux"
+  subnet_ids          = [azurerm_subnet.minio_subnet.id]
   diagnostics {
     log_analytics {
       workspace_id  = azurerm_log_analytics_workspace.minio_law.workspace_id
@@ -47,14 +43,15 @@ resource "azurerm_container_group" "minio_aci_container_group" {
     }
 
     environment_variables = {
-      MINIO_ROOT_USER     = var.minio_root_user
-      MINIO_ROOT_PASSWORD = var.minio_root_password
+      MINIO_ROOT_USER            = var.minio_root_user
+      MINIO_ROOT_PASSWORD        = var.minio_root_password
+      MINIO_BROWSER_REDIRECT_URL = "https://${azurerm_public_ip.minio_pip.fqdn}:9001"
     }
 
     volume {
       name                 = "minio-volume"
       mount_path           = "/data"
-      read_only = false
+      read_only            = false
       storage_account_name = azurerm_storage_account.minio_storage_account.name
       storage_account_key  = azurerm_storage_account.minio_storage_account.primary_access_key
       share_name           = azurerm_storage_share.minio_storage_share.name
