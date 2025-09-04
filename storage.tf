bf4d8fa53f92a40aa43932f7c00d323a05fd9fad
Patrick <pmoore@meshcloud.io>
2025-09-04 20:16:46 +0200

    add variable inputs

--- storage.tf
+++ storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "minio_storage_account" {
-  name                              = "patrickminiostorage"
+  name                              = var.storage_account_name
   resource_group_name               = azurerm_resource_group.minio_aci_rg.name
   location                          = var.location
   account_tier                      = "Standard"
@@ -8,18 +8,18 @@ resource "azurerm_storage_account" "minio_storage_account" {
   access_tier                       = "Hot"
   infrastructure_encryption_enabled = true
   shared_access_key_enabled         = true
-  # https_traffic_only_enabled = false
-  public_network_access_enabled = true
-  allowed_copy_scope            = "AAD"
+  public_network_access_enabled     = true
+  allowed_copy_scope                = "AAD"
+  https_traffic_only_enabled        = true
 
   network_rules {
-    default_action = "Allow"
-    virtual_network_subnet_ids = [azurerm_subnet.minio_ag_subnet.id,azurerm_subnet.minio_subnet.id]
+    default_action             = "Deny"
+    virtual_network_subnet_ids = [azurerm_subnet.minio_ag_subnet.id, azurerm_subnet.minio_subnet.id]
   }
 }
 
 resource "azurerm_storage_share" "minio_storage_share" {
   name               = "miniostorageshare"
   storage_account_id = azurerm_storage_account.minio_storage_account.id
-  quota              = 5 # size in GBs
+  quota              = var.storage_share_size # size in GBs
 }
