a19803270ef812b474a935222a8d3d8db186a4b1
Patrick <pmoore@meshcloud.io>
2025-09-04 19:50:40 +0200

    removed storage subnet. no longer needed

--- network.tf
+++ network.tf
@@ -30,20 +30,11 @@ resource "azurerm_subnet" "minio_ag_subnet" {
   service_endpoints                             = ["Microsoft.Storage"]
 }
 
-resource "azurerm_subnet" "storage_account_subnet" {
-  name                                          = "minio-storage-subnet"
-  resource_group_name                           = azurerm_resource_group.minio_aci_rg.name
-  virtual_network_name                          = azurerm_virtual_network.minio_vnet.name
-  address_prefixes                              = [var.storage_account_subnet_cidr_range]
-  service_endpoints                             = ["Microsoft.Storage"]
-  private_link_service_network_policies_enabled = false
-}
-
 resource "azurerm_public_ip" "minio_pip" {
   name                = "minio-pip"
   location            = azurerm_resource_group.minio_aci_rg.location
   resource_group_name = azurerm_resource_group.minio_aci_rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
-  domain_name_label   = "patrickminiotest"
+  domain_name_label   = var.public_url_domain_name
 }
