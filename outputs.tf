00f3580e87fd881c0e6aafe3926e8c39e11e922b
Patrick <pmoore@meshcloud.io>
2025-09-17 09:58:00 +0200

    updated outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-
-output "platform_tenant_id" {
-  value = azurerm_container_group.minio_aci_container_group.name
-}
 output "console_url" {
   value = "https://${azurerm_public_ip.minio_pip.fqdn}:9001"
 }
