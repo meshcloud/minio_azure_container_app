75153b7e8b9ac65d2d1fab3e60c3fd2ab58dd08e
Florian Nowarre <fnowarre@meshcloud.io>
2025-10-06 09:48:14 +0200

    fix: update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,29 @@
 output "console_url" {
-  value = "https://${azurerm_container_group.minio_aci_container_group.fqdn}"
+  description = "MinIO Web Console URL"
+  value       = "https://${azurerm_container_group.minio_aci_container_group.fqdn}"
 }
-output "api_url" {
-  value = "https://${azurerm_container_group.minio_aci_container_group.fqdn}:9443"
+
+output "s3_api_url" {
+  description = "MinIO S3 API endpoint"
+  value       = "https://${azurerm_container_group.minio_aci_container_group.fqdn}:8443"
 }
-output "minio_username" {
-  value = var.minio_root_user
+
+output "fqdn" {
+  description = "Fully qualified domain name"
+  value       = azurerm_container_group.minio_aci_container_group.fqdn
+}
+
+output "public_ip" {
+  description = "Public IP address"
+  value       = azurerm_container_group.minio_aci_container_group.ip_address
+}
+
+output "mc_alias_command" {
+  description = "MinIO client setup command"
+  value       = "mc alias set myminio https://${azurerm_container_group.minio_aci_container_group.fqdn}:8443 --insecure"
+}
+
+output "storage_account_name" {
+  description = "Azure Storage Account name"
+  value       = azurerm_storage_account.minio_storage_account.name
 }
