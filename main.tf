1110ca49586a779587cb243644f5b5fee7207a58
Florian Nowarre <fnowarre@meshcloud.io>
2025-10-07 10:51:12 +0200

    chore: adding riderct minio

--- main.tf
+++ main.tf
@@ -167,8 +167,9 @@ resource "azurerm_container_group" "minio_aci_container_group" {
     }
 
     environment_variables = {
-      MINIO_UI_BACKEND  = "localhost:9001"
-      MINIO_API_BACKEND = "localhost:9000"
+      MINIO_UI_BACKEND           = "localhost:9001"
+      MINIO_API_BACKEND          = "localhost:9000"
+      MINIO_BROWSER_REDIRECT_URL = "localhost:9001"
     }
 
     liveness_probe {
