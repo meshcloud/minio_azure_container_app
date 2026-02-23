8e28ab10cb1da7c7f765b11d95c233df914b6a57
Florian Nowarre <fnowarre@meshcloud.io>
2026-02-23 16:07:22 +0100

    feat: restructure successfully deployed 2 instances

--- main.tf
+++ main.tf
@@ -1,25 +1,48 @@
-module "seaweedfs" {
+# module "seaweedfs" {
+#   source = "./modules/seaweedfs-instance"
+
+#   namespace              = var.namespace
+#   storage_class_name     = var.storage_class_name
+#   seaweedfs_storage_size = var.seaweedfs_storage_size
+#   mariadb_storage_size   = var.mariadb_storage_size
+#   keycloak_storage_size  = var.keycloak_storage_size
+#   seaweedfs_domain       = var.seaweedfs_domain
+#   keycloak_domain        = var.keycloak_domain
+#   seaweedfs_image        = var.seaweedfs_image
+#   keycloak_image         = var.keycloak_image
+#   mariadb_image          = var.mariadb_image
+#   mariadb_database       = var.mariadb_database
+#   mariadb_user           = var.mariadb_user
+#   keycloak_admin_user    = var.keycloak_admin_user
+#   email_lets_encrypt     = var.email_lets_encrypt
+#   allowed_ip_addresses   = var.allowed_ip_addresses
+#   ingress_class_name     = "bunkerweb-${var.namespace}"
+#   bunkerweb_cluster_ip   = kubernetes_service_v1.bunkerweb_external.spec[0].cluster_ip
+
+#   keycloak_test_user_username = var.keycloak_test_user_username
+#   keycloak_test_user_email    = var.keycloak_test_user_email
+#   opkssh_redirect_uris        = var.opkssh_redirect_uris
+# }
+module "team_alpha" {
   source = "./modules/seaweedfs-instance"
 
-  namespace              = var.namespace
-  storage_class_name     = var.storage_class_name
-  seaweedfs_storage_size = var.seaweedfs_storage_size
-  mariadb_storage_size   = var.mariadb_storage_size
-  keycloak_storage_size  = var.keycloak_storage_size
-  seaweedfs_domain       = var.seaweedfs_domain
-  keycloak_domain        = var.keycloak_domain
-  seaweedfs_image        = var.seaweedfs_image
-  keycloak_image         = var.keycloak_image
-  mariadb_image          = var.mariadb_image
-  mariadb_database       = var.mariadb_database
-  mariadb_user           = var.mariadb_user
-  keycloak_admin_user    = var.keycloak_admin_user
-  email_lets_encrypt     = var.email_lets_encrypt
-  allowed_ip_addresses   = var.allowed_ip_addresses
-  ingress_class_name     = "bunkerweb-${var.namespace}"
-  bunkerweb_cluster_ip   = kubernetes_service_v1.bunkerweb_external.spec[0].cluster_ip
+  namespace            = "seaweedfs"
+  storage_class_name   = "ionos-enterprise-hdd"
+  seaweedfs_domain     = "seaweedfs.ionos.meshcloud.io"
+  keycloak_domain      = "keycloak.ionos.meshcloud.io"
+  email_lets_encrypt   = "fnowarre@meshcloud.io"
+  ingress_class_name   = "bunkerweb"
+  bunkerweb_cluster_ip = "100.64.23.143"
+}
 
-  keycloak_test_user_username = var.keycloak_test_user_username
-  keycloak_test_user_email    = var.keycloak_test_user_email
-  opkssh_redirect_uris        = var.opkssh_redirect_uris
+module "team_beta" {
+  source = "./modules/seaweedfs-instance"
+
+  namespace            = "seaweedfs2"
+  storage_class_name   = "ionos-enterprise-hdd"
+  seaweedfs_domain     = "seaweedfs2.ionos.meshcloud.io"
+  keycloak_domain      = "keycloak2.ionos.meshcloud.io"
+  email_lets_encrypt   = "fnowarre@meshcloud.io"
+  ingress_class_name   = "bunkerweb"
+  bunkerweb_cluster_ip = "100.64.23.143"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,43 +1,43 @@
 output "s3_api_url" {
   description = "SeaweedFS S3 API endpoint (via BunkerWeb ingress)"
-  value       = module.seaweedfs.s3_api_url
+  value       = module.team_alpha.s3_api_url
 }
 
 output "keycloak_url" {
   description = "Keycloak URL"
-  value       = module.seaweedfs.keycloak_url
+  value       = module.team_alpha.keycloak_url
 }
 
 output "keycloak_admin_console_url" {
   description = "Keycloak admin console URL"
-  value       = module.seaweedfs.keycloak_admin_console_url
+  value       = module.team_alpha.keycloak_admin_console_url
 }
 
 output "keycloak_client_secret" {
   description = "Generated Keycloak OIDC client secret for SeaweedFS"
-  value       = module.seaweedfs.keycloak_client_secret
+  value       = module.team_alpha.keycloak_client_secret
   sensitive   = true
 }
 
 output "mariadb_password" {
   description = "Generated MariaDB password"
-  value       = module.seaweedfs.mariadb_password
+  value       = module.team_alpha.mariadb_password
   sensitive   = true
 }
 
 output "keycloak_admin_password" {
   description = "Generated Keycloak admin password"
-  value       = module.seaweedfs.keycloak_admin_password
+  value       = module.team_alpha.keycloak_admin_password
   sensitive   = true
 }
 
 output "keycloak_test_user_password" {
   description = "Generated Keycloak test user password"
-  value       = module.seaweedfs.keycloak_test_user_password
+  value       = module.team_alpha.keycloak_test_user_password
   sensitive   = true
 }
 
 output "aws_cli_configure_command" {
   description = "Command to configure AWS CLI for SeaweedFS S3"
-  value       = module.seaweedfs.aws_cli_configure_command
+  value       = module.team_alpha.aws_cli_configure_command
 }
