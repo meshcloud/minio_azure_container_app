99c6314c49f2e229c05ea605b28ead7b134abc4a
Florian Nowarre <fnowarre@meshcloud.io>
2026-02-23 14:17:45 +0100

    feat: test adding as an module

--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
 provider "kubernetes" {
-  config_path    = var.kubeconfig_path
-  config_context = var.kubeconfig_context
+  # config_path    = var.kubeconfig_path
+  # config_context = var.kubeconfig_context
 }
 
 provider "helm" {
   kubernetes {
-    config_path    = var.kubeconfig_path
-    config_context = var.kubeconfig_context
+    # config_path    = var.kubeconfig_path
+    # config_context = var.kubeconfig_context
   }
 }
