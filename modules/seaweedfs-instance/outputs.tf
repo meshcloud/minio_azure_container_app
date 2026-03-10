b1cd6d147eb8c0df699280b7d2c301135455c8ca
Florian Nowarre <fnowarre@meshcloud.io>
2026-03-10 17:13:10 +0100

    feat: true sensitive

--- modules/seaweedfs-instance/outputs.tf
+++ modules/seaweedfs-instance/outputs.tf
@@ -28,13 +28,13 @@ output "mariadb_password" {
 output "keycloak_admin_password" {
   description = "Generated Keycloak admin password"
   value       = random_password.keycloak_admin_password.result
-  #sensitive   = true
+  sensitive   = true
 }
 
 output "keycloak_test_user_password" {
   description = "Generated Keycloak test user password"
   value       = random_password.keycloak_test_user_password.result
-  #sensitive   = true
+  sensitive   = true
 }
 
 output "aws_cli_configure_command" {
